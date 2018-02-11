3858ec972a7fa36552056deb795074f94e5c4775
b4dnewz <filippo@codekraft.it>
2018-02-11 03:22:31 +0100

    Socketio service
    This service holds the live feed entries

---
+++ src/app/socket.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+import * as io from 'socket.io-client';
+
+import { environment } from '../environments/environment';
+
+import { LogItem } from './log-item'
+
+@Injectable()
+export class SocketService {
+
+  // Maximum number of displayed keylog entries
+  maxLogs: number = 1000;
+
+  // The socket connection
+  socket: any;
+
+  // Array of keylog entries
+  items: Array<LogItem> = [];
+
+  // Counter for unread entries
+  counter: number = 0;
+  counting: Subject<number> = new Subject<number>();
+  isCounting: Boolean = true;
+
+  constructor() {
+    // Connect to socket server in admin namespace
+    // force the reconnection and infinity attempts
+    this.socket = io(`${environment.SERVER_URL}/admin`, {
+      autoConnect: true,
+      reconnection: true,
+      reconnectionAttempts: 'Infinity'
+    })
+
+    // Listen to keylog event and populate items
+    this.socket.on('keylog', (data) => {
+      this.items.push(data);
+
+      // If current length exceed the maximum, remove from top
+      if (this.items.length > this.maxLogs) {
+        this.items.shift();
+      }
+
+      // If is counting increment new items counter
+      if (this.isCounting) {
+        this.counter += 1
+        this.counting.next(this.counter)
+      }
+    })
+  }
+
+  stopCounting () {
+    this.isCounting = false;
+    this.counter = 0
+    this.counting.next(this.counter)
+  }
+
+  startCounting () {
+    this.isCounting = true;
+  }
+
+}
