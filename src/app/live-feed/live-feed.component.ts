5d227a00b7c1410b289010a76e92c59569c1e1fb
b4dnewz <filippo@codekraft.it>
2018-02-11 20:29:35 +0100

    Added liveFeed component
    This component displays the live feed of keylogs from active clients

--- src/app/live-feed/live-feed.component.ts
+++ src/app/live-feed/live-feed.component.ts
@@ -1,13 +1,6 @@
-import { Component, IterableDiffers, OnInit, ViewChild } from '@angular/core';
-import { NgZone } from '@angular/core';
-
-import * as io from 'socket.io-client';
-
-import { environment } from '../../environments/environment';
+import { Component, IterableDiffers, OnInit } from '@angular/core';
 
 import { SocketService } from '../socket.service'
-
-// Import interfaces
 import { LogItem } from '../log-item'
 
 @Component({
@@ -17,44 +10,17 @@ import { LogItem } from '../log-item'
 })
 
 export class LiveFeedComponent implements OnInit {
-  @ViewChild('channels') channels;
 
   // Entries filter
   textFilter: string = '';
-  showFilter: Boolean;
-
-  socket: any;
 
   // Entries array
-  differ: any;
   items: Array<LogItem> = [];
 
-  // Unique clients hostnames
-  hostnames: Array<String> = [];
+  // Array of selected hostnames to filter
   selectedHostnames: Array<String> = [];
 
-  constructor(
-    private iterableDiffers: IterableDiffers,
-    private socketService: SocketService
-  ) {
-    this.differ = this.iterableDiffers.find([]).create(null);
-    this.socket = socketService.socket
-  }
-
-  // Filter entries using given hostname selection
-  onSelectionChange() {
-    this.selectedHostnames = this.channels.selectedOptions.selected.map(item => item.value)
-  }
-
-  // On entries change update unique hostnames
-  ngDoCheck() {
-    let changes = this.differ.diff(this.items);
-    if (changes) {
-      this.hostnames = Array.from(
-        new Set(this.items.map(item => item.hostname)).values()
-      )
-    }
-  }
+  constructor(private socketService: SocketService) {}
 
   // Bind socket service data to entries
   ngOnInit() {
@@ -62,6 +28,7 @@ export class LiveFeedComponent implements OnInit {
     this.socketService.stopCounting();
   }
 
+  // When route is left restart counting for new entries (unread)
   ngOnDestroy () {
     this.socketService.startCounting();
   }
